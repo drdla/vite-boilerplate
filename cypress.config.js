fa7e187ffe8eab8ff558982d27929e259c8662d7
Dominik Rodler <dominik.rodler@gmail.com>
2024-06-18 10:05:21 +0200

    build: set up Cypress e2e testing

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+import vitePreprocessor from 'cypress-vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      on(
+        'file:preprocessor',
+        vitePreprocessor({
+          configFile: path.resolve(__dirname, './vite.config.ts'),
+          mode: 'development',
+        }),
+      );
+    },
+  },
+});
