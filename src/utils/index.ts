20306620538ec5bb7285e2578f2140ed321b79e0
Dominik Rodler <dominik.rodler@gmail.com>
2024-06-21 10:57:10 +0200

    feat: add lazyLoad util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ import i18nLib from './i18n';
 export const i18n = i18nLib;
 
 export * from './i18n';
+export * from './lazyLoad';
+export * from './tailwind';
---
+++ src/utils/lazyLoad.ts
@@ -0,0 +1,30 @@
+import { ComponentType, lazy, LazyExoticComponent } from 'react';
+
+type LazyLoadReturn<T> =
+  | LazyExoticComponent<ComponentType<T>>
+  | Promise<{ default: T }>;
+
+/**
+ * Lazily loads a module from the given import path and either returns
+ * a) the default export or
+ * b) a given named export as default export.
+ *
+ * @param {string} importPath - The path of the module to be imported.
+ * @param {string} namedExport - The name of the named export to be extracted from the module.
+ * @return {LazyLoadReturn<any>} A promise that resolves to the extracted named export from the module.
+ */
+export const lazyLoad = <T = unknown>(
+  importPath: string,
+  namedExport?: string,
+): LazyLoadReturn<T> =>
+  lazy(() => {
+    const promise = import(importPath);
+
+    if (!namedExport) {
+      return promise;
+    }
+
+    return promise.then((module: Record<string, T>) => ({
+      default: module[namedExport],
+    }));
+  });
